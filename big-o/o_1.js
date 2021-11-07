5c67a9e8836a60a503e66adc65e112fde4b193e0
kangho <83585224+hallokangho@users.noreply.github.com>
2021-11-07 07:08:01 +0000

    Update : O(1) test

---
+++ big-o/o_1.js
@@ -0,0 +1,7 @@
+const elements = ['a', 'b', 'c'];
+
+function getElements(number){
+  console.log(elements[number]);
+}
+
+getElements(1); // O(1)
--- big-o/o_n.js
+++ big-o/o_n.js
@@ -4,7 +4,8 @@ const elements = ['c', 'b', 'a']
 function findTarget(target){
   for(let i=0; i<elements.length; i++){
     if(target == elements[i]){
-      console.log('Target found: ' + i)
+      console.log('Target found: ' + i);
+      break;
     }
   }
 }
