c6b34d3f3bdd456f294eac80978285e4fa19ccdc
kangho <83585224+hallokangho@users.noreply.github.com>
2021-11-19 13:59:18 +0000

    Update : linked list printList method

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -9,50 +9,67 @@ class Node{
 
 class LinkedList{
   constructor(value){
-    this.node=new Node(value);
-    this.tail=this.node.head;
-    this.length=1;
+    this.node = new Node(value);
+    this.tail = this.node.head;
+    this.length = 1;
   }
 
   //O(1)
   append(value){
-    const newNode=new Node(value);
-    this.tail.next=newNode;
-    this.tail=newNode;
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
     this.length++;
     return this;
   }
 
   //O(1)
   prepend(value){
-    const newNode=new Node(value);
-    newNode.next=this.node.head;
-    this.node.head=newNode;
+    const newNode = new Node(value);
+    newNode.next = this.node.head;
+    this.node.head = newNode;
     this.length++;
     return this;
   }
 
   //O(n)
   insert(index, value){
-    if(index>=this.length){
+    if(index >= this.length){
       return this.append(value);
     }
 
-    let newNode=new Node(value);
-    const leader= this.traverse(index-1);
-    const holdingPointer= leader.next;
-    leader.next=newNode;
-    newNode.next=holdingPointer;
+    let newNode = new Node(value);
+    const leader = this.traverse(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
     this.length++;
+  }
+
+  //O(n) 
+  printList(){
+    let array = new Array();
+    let currentNode = this.node.head.next;
+    let counter = 0;
+
+    array.push(this.node.head.value); //Need to check
+
+    while(counter < this.length-1){
+      array.push(currentNode.head.value);
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return array;
   }
 
   //O(n)
   traverse(index){
-    let counter=0;
-    let currentNode=this.node.head;
+    let counter = 0;
+    let currentNode = this.node.head;
 
-    while(counter!==index){
-      currentNode=currentNode.next;
+    while(counter !== index){
+      currentNode = currentNode.next;
       counter++;
     }
     
@@ -60,10 +77,11 @@ class LinkedList{
   }
 }
 
-const list=new LinkedList('A');
+const list = new LinkedList('A');
 list.append('B');
 list.append('C');
 list.append('D');
+console.log(list.printList());
 
-list.insert(2, '1');
-console.log(JSON.stringify(list));
+// list.insert(2, '1');
+// console.log(JSON.stringify(list));
