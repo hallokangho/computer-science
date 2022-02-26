da40ad1a877657c7d10315a2160612e8373e0506
kangho <83585224+hallokangho@users.noreply.github.com>
2022-02-26 10:13:29 +0000

    Update : plus-one leetcode

--- leetCode/plus-one.js
+++ leetCode/plus-one.js
@@ -1,17 +1,29 @@
 /**
  * @param {number[]} digits
  * @return {number[]}
- * https://leetcode.com/problems/plus-one/
+ * https://leetcode.com/problems/plus-one
  */
-var plusOne = function(digits) {
-    console.log('digits: ', digits)
-    
+var plusOne = function(digits) {    
     let output = digits
 
     if(digits[digits.length-1] < 9){
         output[digits.length-1] ++
         return output
-    }    
+    }
     
-    // Need to progress
+    for(let i=digits.length-1; i>=0; i--){        
+        if(digits[i] == 9){
+            output[i] = 0
+        }
+        
+        if(digits[i-1] == undefined){
+            output.splice(i, 0, 1)
+            break
+        }else if(digits[i-1] < 9){
+            digits[i-1] = digits[i-1]+1
+            break
+        }
+    }
+    
+    return output
 };
